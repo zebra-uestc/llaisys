870393c7e97c2f8ae66a5bc31cb411bf5ce910b7
tianyuxbear <tianyuxbear@gmail.com>
2025-09-15 13:40:11 +0800

    feat: enable AVX512 flags on Linux x86_64 for CPU ops

--- xmake/cpu.lua
+++ xmake/cpu.lua
@@ -20,6 +20,18 @@ target("llaisys-ops-cpu")
         add_cxflags("-fPIC", "-Wno-unknown-pragmas")
     end
 
+    -- For AVX512
+    if is_plat("linux")and is_arch("x86_64") then
+        add_cxflags("-mavx512f -mfma -mf16c")
+        add_mxflags("-mavx512f -mfma -mf16c")
+    end
+    
+    -- For AVX2
+    -- if is_plat("linux")and is_arch("x86_64") then
+    --     add_cxflags("-mavx2 -mfma -mf16c")
+    --     add_mxflags("-mavx2 -mfma -mf16c")
+    -- end
+
     add_packages("openmp", "openblas")
 
     add_files("../src/ops/*/cpu/*.cpp")
