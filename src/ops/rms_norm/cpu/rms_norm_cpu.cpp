f5b70442a61eb9d49892e8fcac3f5c8770f1510d
AthBe1337 <prayer@athbe.cn>
2025-12-17 15:46:59 +0800

    feat(rms_norm): Add int8 support with AVX512 optimizations for RMS normalization on CPU

--- src/ops/rms_norm/cpu/rms_norm_cpu.cpp
+++ src/ops/rms_norm/cpu/rms_norm_cpu.cpp
@@ -53,24 +53,50 @@ void rms_norm_(T *out, const T *in, const T *weight, float eps, size_t nrow, siz
 
         } else if constexpr (std::is_same_v<T, int8_t>) {
             int32_t square_sum = llaisys::ops::cpu::sdot_int8(in_t, in_t, ncol);
-
             float mean = static_cast<float>(square_sum) / static_cast<float>(ncol);
-            
             float rms = std::sqrt(mean + eps);
             float scale = 1.0f / rms;
 
-            for (size_t j = 0; j < ncol; ++j) {
+            size_t j = 0;
+
+#if defined(__AVX512F__) && defined(__AVX512BW__)
+            __m512 v_scale = _mm512_set1_ps(scale);
+            __m512i v_max = _mm512_set1_epi32(127);
+            __m512i v_min = _mm512_set1_epi32(-128);
+
+            for (; j + 15 < ncol; j += 16) {
+                __m128i v_in_128 = _mm_loadu_si128((const __m128i*)(in_t + j));
+                __m128i v_w_128  = _mm_loadu_si128((const __m128i*)(weight + j));
+
+                __m512i v_in_i32 = _mm512_cvtepi8_epi32(v_in_128);
+                __m512i v_w_i32  = _mm512_cvtepi8_epi32(v_w_128);
+                
+                __m512 v_in_f32 = _mm512_cvtepi32_ps(v_in_i32);
+                __m512 v_w_f32  = _mm512_cvtepi32_ps(v_w_i32);
+
+                __m512 v_res = _mm512_mul_ps(v_in_f32, v_w_f32);
+                v_res = _mm512_mul_ps(v_res, v_scale);
+
+                __m512i v_res_i32 = _mm512_cvtps_epi32(v_res);
+
+                v_res_i32 = _mm512_min_epi32(v_res_i32, v_max);
+                v_res_i32 = _mm512_max_epi32(v_res_i32, v_min);
+
+                __m128i v_out_128 = _mm512_cvtepi32_epi8(v_res_i32);
+                _mm_storeu_si128((__m128i*)(out_t + j), v_out_128);
+            }
+#endif
+
+            for (; j < ncol; ++j) {
                 float w_val = static_cast<float>(weight[j]);
                 float x_val = static_cast<float>(in_t[j]);
-                
                 float res = x_val * w_val * scale;
-                
-                res = std::round(res);
-                
-                if (res > 127.0f) res = 127.0f;
-                if (res < -128.0f) res = -128.0f;
 
-                out_t[j] = static_cast<int8_t>(res);
+                float rounded = (res >= 0.0f) ? (res + 0.5f) : (res - 0.5f);
+                if (rounded > 127.0f) rounded = 127.0f;
+                if (rounded < -128.0f) rounded = -128.0f;
+
+                out_t[j] = static_cast<int8_t>(rounded);
             }
         } else {
             T rms{}, square_sum{}, avg_square_sum{};
